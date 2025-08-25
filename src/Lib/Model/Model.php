31cf250ea3945486d2e9a4b1643a3bd684e426c4
kennyaja <121273982+kennyaja@users.noreply.github.com>
2025-08-26 00:21:36 +0700

    add model class

---
+++ src/Lib/Model/Model.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Lib\Model;
+
+use PDO;
+
+class Model {
+	public $db;
+	function __construct() {
+		$db_driver = $_ENV["db_driver"];
+		$db_hostname = $_ENV["db_hostname"] ?? "127.0.0.1";
+		$db_port = $_ENV["db_port"] ?? "3306";
+		$db_username = $_ENV["db_username"];
+		$db_password = $_ENV["db_password"];
+		$db_name = $_ENV["db_name"];
+		$this->db = new PDO("$db_driver:host=$db_hostname;port=$db_port;dbname=$db_name", $db_username, $db_password); 
+	}
+}
