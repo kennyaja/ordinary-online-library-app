4010cabefcb1afc3a1c19e1b013008585341e50e
kennyaja <121273982+kennyaja@users.noreply.github.com>
2025-08-27 14:35:20 +0700

    add login for cashier and admin accounts

--- src/Controllers/Login.php
+++ src/Controllers/Login.php
@@ -3,6 +3,8 @@
 namespace App\Controllers;
 
 use App\Lib\View\View;
+use App\Models\AdminsModel;
+use App\Models\CashiersModel;
 use App\Models\UsersModel;
 
 class Login {
@@ -34,13 +36,24 @@ class Login {
 	public function api_login() {
 		header("Content-Type: application/json");
 
-		// $database = new Database();
-		// $check_query = $database->db->prepare("SELECT * FROM users WHERE username=:username");
-		// $check_query->execute(["username" => $_POST["username"]]);
-		// 
-		// $user_data = $check_query->fetch();
 		$users_model = new UsersModel();
+		$cashiers_model = new CashiersModel();
+		$admins_model = new AdminsModel();
+
 		$user_data = $users_model->getFirst(condition: "username=?", params: [$_POST["username"]]);
+		$user_role = "user";
+		
+		// first iteration (dang it why does the teacher want a discrete table for each account role)
+		if (!$user_data) {
+			$user_data = $cashiers_model->getFirst(condition: "username=?", params: [$_POST["username"]]);
+			$user_role = "cashier";
+		}
+		
+		// second iteration
+		if (!$user_data) {
+			$user_data = $admins_model->getFirst(condition: "username=?", params: [$_POST["username"]]);
+			$user_role = "admin";
+		}
 
 		if (!$user_data || !password_verify($_POST["password"], $user_data["password_hash"])) {
 			return json_encode(["status" => "err", "error" => "Username or password is incorrect"]);
@@ -48,26 +61,12 @@ class Login {
 
 		$_SESSION["username"] = $user_data["username"];
 		$_SESSION["user_id"] = $user_data["id"];
+		$_SESSION["user_role"] = $user_role;
 
 		header("location: /");
-		// return json_encode(["status" => "ok"]);
-
-		// if () {
-		// 	$_SESSION["error_msg"] = "Username or password is incorrect";
-		// 	header("location: /login");
-		// 	return;
-		// }
-		// header("location: /");
-		// return;
 	}
 
 	public function api_signup() {
-		// $database = new Database();
-		// $insert_query = $database->db->prepare("INSERT INTO users (username, password_hash, email) VALUES (:username, :password_hash, :email)");
-		// $insert_query->execute([
-		// 	"username" => $_POST["username"], 
-		// 	"password_hash" => password_hash($_POST["password"], PASSWORD_DEFAULT), 
-		// 	"email" => $_POST["email"] ?? ""]);
 		$users_model = new UsersModel();
 		$users_model->insert([
 			"username" => $_POST["username"], 
