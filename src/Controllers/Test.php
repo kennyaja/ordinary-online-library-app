6a8de5adc59db43dc426d5be9f34f99062bf1d2d
kennyaja <121273982+kennyaja@users.noreply.github.com>
2025-08-30 22:07:18 +0700

    it works :D

--- src/Controllers/Test.php
+++ src/Controllers/Test.php
@@ -5,14 +5,19 @@ namespace App\Controllers;
 use App\Lib\View\View;
 use App\Models\UsersModel;
 
+use App\Lib\HTTP\HTTPHeader;
+
 class Test {
 	public function index() {
+		$http_header = new HTTPHeader();
 		$view = new View(
 			"test.php", 
 			["a" => var_export((new UsersModel())->getFirst("username"), true)],
 			"dog cument",
 			(new View("components/metadata.php"))->get_raw(),
 		);
+
+		$http_header->location = "/alkdsjksaldjakld";
 
 		return $view->get();
 	} 
