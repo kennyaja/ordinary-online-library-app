b04ad3251c2152644a6491a467b9a53cfa392fba
kennyaja <121273982+kennyaja@users.noreply.github.com>
2025-08-30 23:52:01 +0700

    make data nullable in view class

--- src/Lib/View/View.php
+++ src/Lib/View/View.php
@@ -5,12 +5,14 @@ namespace App\Lib\View;
 use App\Lib\Directory\Directory;
 
 class View {
-	public static function get(string $view_path, array $data) {
+	public static function get(string $view_path, array|null $data = null) {
 		ob_start();
 
-		foreach ($data as $key => $value) {
-			// dynamic variable shenanigans
-			$$key = $value;
+		if ($data != null) {
+			foreach ($data as $key => $value) {
+				// dynamic variable shenanigans
+				$$key = $value;
+			}
 		}
 
 		require(Directory::get_full_path("src/Views/" . $view_path));
