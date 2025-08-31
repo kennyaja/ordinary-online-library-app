be1340b898d20a208dc92d2cd59066a8f25fab6c
kennyaja <121273982+kennyaja@users.noreply.github.com>
2025-09-01 05:22:19 +0700

    apply new view method to every view file

--- src/Views/index.php
+++ src/Views/index.php
@@ -1,2 +1,14 @@
-<?= $_SESSION["username"] ?? "log in buddy" ?> (<?= $_SESSION["user_role"] ?? "" ?>)
-<a href="/api/logout" class="text-blue-500">log out</a>
+<?php
+use App\Lib\View\View;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<?= View::get("components/metadata.php", ["title" => $title]) ?>
+</head>
+<body>
+	<?= $_SESSION["username"] ?? "log in buddy" ?> (<?= $_SESSION["user_role"] ?? "" ?>)
+	<a href="/api/logout" class="text-blue-500">log out</a>
+</body>
+</html>
