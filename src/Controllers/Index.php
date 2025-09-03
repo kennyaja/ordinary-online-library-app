34edccabfc4bf421a5d8ea85a56761ba8781c006
kennyaja <121273982+kennyaja@users.noreply.github.com>
2025-09-03 13:59:53 +0700

    use http header class for index

--- src/Controllers/Index.php
+++ src/Controllers/Index.php
@@ -2,12 +2,19 @@
 
 namespace App\Controllers;
 
+use App\Lib\HTTP\HTTPHeader;
 use App\Lib\View\View;
 
 class Index {
+	var $http_header;
+
+	function __construct() {
+		$this->http_header = new HTTPHeader();
+	}
+
 	public function index() {
 		if (!isset($_SESSION["user_id"])) {
-			header("location: /login");
+			$this->http_header->location = "login";
 			return;
 		}
 
