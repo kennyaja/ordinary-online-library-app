6dd27b98e934b89a81961a2579840381c6a5c912
kennyaja <121273982+kennyaja@users.noreply.github.com>
2025-09-23 01:09:18 +0700

    :thumbs_up: Umazing!

--- src/Views/admin/index.php
+++ src/Views/admin/index.php
@@ -12,7 +12,16 @@ use App\Lib\View\View;
 	<div class="grid grid-flow-col grid-cols-[auto_1fr] h-[calc(100dvh-3rem)]">
 		<?= View::get("components/admin/sidebar.php") ?>
 		<div class="px-4 py-5">
-			lol
+			<button class="bg-uma-green px-4 py-2 rounded-md shadow-md text-white text-shadow-md" id="umazing_button">&#x1F44D; Umazing!</button>
+
+			<script>
+				const umazing_button = document.getElementById("umazing_button");
+				umazing_button.onclick = () => {
+					umazing_button.classList.remove("bg-uma-green");
+					umazing_button.classList.add("bg-uma-pink");
+					umazing_button.innerHTML = "&#x1F44D; Umazed!";
+				}
+			</script>
 		</div>
 	</div>
 </body>
