fba93a108d838f0bb73b6407a6cd20237bc2ad63
kennyaja <121273982+kennyaja@users.noreply.github.com>
2025-09-26 00:46:02 +0700

    refactor book update

--- src/Controllers/Books.php
+++ src/Controllers/Books.php
@@ -69,12 +69,13 @@ class Books {
 
 		$errors = [];
 
+		$current_data = $this->books_model->where("id", $_POST["id"])->getFirst();
+
 		if (
 			isset($_FILES["content_pdf_file"]) && 
 			!empty($_FILES["content_pdf_file"]) && 
 			$_FILES["content_pdf_file"]["error"] != 4
 		) {
-			var_dump($_FILES["content_pdf_file"]);
 			if ($_FILES["content_pdf_file"]["type"] != "application/pdf") {
 				$errors["content_pdf_file"] = "Uploaded file has type of " . $_FILES["content_pdf_file"]["type"] . ", which is not 'application/pdf'";
 			}
@@ -84,32 +85,31 @@ class Books {
 			}
 
 			$pdf_file_path = sprintf("uploads/pdf/%d_%s", time(), $_FILES["content_pdf_file"]["name"]);
-			$image_file_path = sprintf("uploads/img/book_cover/%d_%s", time(), $_FILES["image_file"]["name"]);
-
 			move_uploaded_file($_FILES["content_pdf_file"]["tmp_name"], $pdf_file_path);
 
-			if (
-				isset($_FILES["image_file"]) && 
-				!empty($_FILES["image_file"]) && 
-				$_FILES["image_file"]["error"] != 4
-			) {
-				move_uploaded_file($_FILES["image_file"]["tmp_name"], $image_file_path);
-			}
-
 			$this->books_model->update([
-				"title" => $_POST["title"],
-				"author" => $_POST["author"],
-				"description" => $_POST["description"],
 				"content_cdn_url" => "{server_addr}/" . $pdf_file_path,
-				"image_url" => "{server_addr}/" . $image_file_path,
-			])->where("id", $_POST["id"])->execute();
-		} else {
-			$this->books_model->update([
-				"title" => $_POST["title"],
-				"author" => $_POST["author"],
-				"description" => $_POST["description"],
 			])->where("id", $_POST["id"])->execute();
 		}
+
+		if (
+			isset($_FILES["image_file"]) && 
+			!empty($_FILES["image_file"]) && 
+			$_FILES["image_file"]["error"] != 4
+		) {
+			$image_file_path = sprintf("uploads/img/book_cover/%d_%s", time(), $_FILES["image_file"]["name"]);
+			move_uploaded_file($_FILES["image_file"]["tmp_name"], $image_file_path);
+
+			$this->books_model->update([
+				"image_url" => "{server_addr}/" . $image_file_path,
+			])->where("id", $_POST["id"])->execute();
+		}
+
+		$this->books_model->update([
+			"title" => $_POST["title"] ?? $current_data["title"],
+			"author" => $_POST["author"] ?? $current_data["author"],
+			"description" => $_POST["description"] ?? $current_data["description"],
+		])->where("id", $_POST["id"])->execute();
 
 		return json_encode(["status" => "ok"]);
 	}
